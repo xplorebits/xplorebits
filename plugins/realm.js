c6d142f3c9eab7a6121fac0bb9627dc5a020867b
saikksub <saikksub@gmail.com>
2023-01-31 00:04:54 +0530

    Adds firebase

--- plugins/realm.js
+++
@@ -1,8 +0,0 @@
-import * as Realm from "realm-web";
-
-export default defineNuxtPlugin(() => {
-  const app = new Realm.App({ id: "mychatra-witjs" });
-  return {
-    provide: { rApp: app },
-  };
-});
